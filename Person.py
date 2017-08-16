a15cc7890e849a0b7e55b3fd5cd66e3f5628284a
PETER MALCOLM <agtychefortuna@gmail.com>
2017-08-16 11:38:30 -0700

    person.py
    Class defining a function

---
+++ Person.py
@@ -0,0 +1,7 @@
+class Person:
+
+    def __init__(self, first_name, last_name, year_of_birth, gender):
+        self.first_name = first_name
+        self.last_name = last_name
+        self. year_of_birth = year_of_birth
+        self.gender = gender
